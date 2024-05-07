295b1f1ed380a14398a104765999a5421f169d76
HarshGoel22 <hgoel0260@gmail.com>
2024-05-08 00:55:41 +0530

    commit-3.11

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,4 +1,6 @@
-const BASE_URL = "http://localhost:4000/api/v1"
+require("dotenv").config();
+
+const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:4000/api/v1"
 console.log(BASE_URL)
 // AUTH ENDPOINTS
 export const endpoints = {
